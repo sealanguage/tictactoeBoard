139a4af64911cb35a14f15b3501a47f6a322c99b
Elaine Froehlich <elaine.froehlich@gmail.com>
2019-01-29 02:31:55 +0000

    added logic to place a character in the cells of the array

--- tictactoe3.js
+++ tictactoe3.js
@@ -20,7 +20,7 @@ process.stdout.write(dash + plus + dash + plus + dash + "\n");
 process.stdout.write(space + vbar + space + vbar + space + "\n" + "\n" + "\n");
 
 
-
+// this is the board in a multidimensional array
 var a = [["X", vbar, "X", vbar, "X"], 
     [dash, plus, dash, plus, dash], 
     ["X", vbar, "X", vbar, "X"], 
@@ -29,9 +29,20 @@ var a = [["X", vbar, "X", vbar, "X"],
     
 var gameRaw = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     
+a[0][0] = gameRaw[0];
+a[0][2] = gameRaw[1];
+a[0][4] = gameRaw[2];
+
+a[2][0] = gameRaw[3];
+a[2][2] = gameRaw[4];
+a[2][4] = gameRaw[5];
+
+a[4][0] = gameRaw[6];
+a[4][2] = gameRaw[7];
+a[4][4] = gameRaw[8];
 
 console.log("reading array a \n");
- 
+// drawing the board 
 for (var i in a) 
 {
   console.log(a[i].join(""));
