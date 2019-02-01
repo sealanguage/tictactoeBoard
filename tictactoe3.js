fc5d6061f722476a8f6bac2d3733658505118ac0
Elaine Froehlich <elaine.froehlich@gmail.com>
2019-02-01 01:55:42 +0000

    working on importBoard function

--- tictactoe3.js
+++ tictactoe3.js
@@ -1,9 +1,4 @@
-// console.log(String.fromCharCode(8212));
-// console.log(String.fromCharCode(43));
-// console.log(String.fromCharCode(124));
 
-
-// var space = (String.fromCharCode(32));
 var i = i;
 var j = j;
 var k = k;
@@ -26,20 +21,41 @@ var a = [["X", vbar, "X", vbar, "X"],
     ["X", vbar, "X", vbar, "X"], 
     [dash, plus, dash, plus, dash], 
     ["X", vbar, "X", vbar, "X"]];
-    
+
+// this array is data that the code below loads into the board
 var gameRaw = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    
-a[0][0] = gameRaw[0];
-a[0][2] = gameRaw[1];
-a[0][4] = gameRaw[2];
+  
+// // this puts the values from gameRow array into the board
+// a[0][0] = gameRaw[0];
+// a[0][2] = gameRaw[1];
+// a[0][4] = gameRaw[2];
 
-a[2][0] = gameRaw[3];
-a[2][2] = gameRaw[4];
-a[2][4] = gameRaw[5];
+// a[2][0] = gameRaw[3];
+// a[2][2] = gameRaw[4];
+// a[2][4] = gameRaw[5];
 
-a[4][0] = gameRaw[6];
-a[4][2] = gameRaw[7];
-a[4][4] = gameRaw[8];
+// a[4][0] = gameRaw[6];
+// a[4][2] = gameRaw[7];
+// a[4][4] = gameRaw[8];
+// var string = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+function importBoard(string[9]) {
+    return string;
+}
+
+// change gameRow to importBoard
+a[0][0] = string[0];
+a[0][2] = string[1];
+a[0][4] = string[2];
+
+a[2][0] = string[3];
+a[2][2] = string[4];
+a[2][4] = string[5];
+
+a[4][0] = string[6];
+a[4][2] = string[7];
+a[4][4] = string[8];
+
 
 console.log("reading array a \n");
 // drawing the board 
